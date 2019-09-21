f872d3a7615cceb76d9225c5b07779d248ffe53d
ichtestemalwieder <54768881+ichtestemalwieder@users.noreply.github.com>
2019-09-21 20:07:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var http = require('http');
+'use strict';
 
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi !');
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
 });
-server.listen(8080);
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
